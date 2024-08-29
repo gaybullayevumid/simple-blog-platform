6d442572b6028bdad332741e232e898cf4ee5fac
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-08-29 12:51:20 +0500

    feat: template file data has been entered into the template

--- base/views.py
+++ base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def getPosts(request):
+    template_name = 'pages/blog.html'
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request=request, template_name=template_name, context=context)
