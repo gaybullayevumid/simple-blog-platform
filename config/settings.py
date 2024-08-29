efb2765830a6c5cd1624b8da7d6d5d36f317a7d1
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-08-29 21:18:38 +0500

    fix: remove jazzmin

--- config/settings.py
+++ config/settings.py
@@ -20,15 +20,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    # installed app
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     # local apps
-    'base.apps.BaseConfig'    
+    'base.apps.BaseConfig',
 
 ]
 
