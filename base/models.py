2ee1cc1fb05d82a5ec2cfe7222124a2dada64f67
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-08-29 11:35:40 +0500

    feat: create models

--- base/models.py
+++ base/models.py
@@ -6,3 +6,10 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     body = models.TextField()
     image = models.FileField(upload_to='media/images')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
