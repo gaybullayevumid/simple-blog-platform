3e63c355940392210f0bcd729306b070d4b24e43
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-08-29 11:30:29 +0500

    feat: create model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    image = models.FileField(upload_to='media/images')
