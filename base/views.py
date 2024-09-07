a2557cf352855806335ca393e3feab62fce56396
gaybullayevumid <umidgaybullayev955@egmail.com>
2024-09-07 19:27:06 +0500

    fix: fix views

--- base/views.py
+++ base/views.py
@@ -9,12 +9,12 @@ def getPosts(request):
     context = {'posts': posts}
     return render(request=request, template_name=template_name, context=context)
 
-def post_detail(request, pk):
-    template_name = 'pages/blog_detail.html'
-    post = Post.objects.get(pk=pk)
-    last_posts = Post.objects.order_by('-created_on')
-    context = {
-                'post': post,
-                'last_posts':last_posts
-               }
-    return render(request=request, template_name=template_name, context=context)
+# def post_detail(request, pk):
+#     template_name = 'pages/blog_detail.html'
+#     post = Post.objects.get(pk=pk)
+#     last_posts = Post.objects.order_by('-created_on')
+#     context = {
+#                 'post': post,
+#                 'last_posts':last_posts
+#                }
+#     return render(request=request, template_name=template_name, context=context)
